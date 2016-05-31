84af23004aea3fa557840793ed33349c2edaae4c
Guillaume Baudusseau <guillaume.baudusseau@gmail.com>
2016-05-31 10:37:46 -0400

    remove useless line

--- app.js
+++ app.js
@@ -137,7 +137,6 @@ projectsList.map(fetchProjectData);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
